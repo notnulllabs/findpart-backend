485cac25126d3056dd0f326dc8b1a32f1355af7a
umid4892 <umid4892@gmail.com>
2017-11-18 14:03:36 +0900

    1

--- find-party/src/main/java/com/find/party/board/entity/Board.java
+++ find-party/src/main/java/com/find/party/board/entity/Board.java
@@ -1,7 +1,44 @@
 package com.find.party.board.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+@Entity
 public class Board {
+	
+	@Id
+	private String boardId;
+	
+	private String title;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private String regDate;
+	
+	
+	public String getBoardId() {
+		return boardId;
+	}
+	public void setBoardId(String boardId) {
+		this.boardId = boardId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
 
+
+	
+	
+	
+	
 }
